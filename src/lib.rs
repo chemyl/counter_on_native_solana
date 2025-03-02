5bd148b9fbced937c616f91749e266a345631bb5
Anatolii Maltsev <chemyl.inc@gmail.com>
2025-03-02 12:52:26 +0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,113 @@
+use crate::instructions::CounterInstruction;
+use borsh::BorshDeserialize;
+use borsh::BorshSerialize;
+// use borsh_derive::BorshDeserialize;
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+pub mod instructions;
+
+#[derive(Debug, BorshDeserialize, BorshSerialize)]
+pub struct CounterAccount {
+    pub counter: u32,
+}
+
+entrypoint!(process_instruction);
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("program entry point");
+
+    let instruction: CounterInstruction = CounterInstruction::unpack(instruction_data)?;
+
+    let accounts_iter = &mut accounts.iter();
+    let account = next_account_info(accounts_iter)?;
+
+    let mut counter_account = CounterAccount::try_from_slice(&account.data.borrow())?;
+
+    match instruction {
+        CounterInstruction::Increment => counter_account.counter += 1,
+        CounterInstruction::Decrement => counter_account.counter -= 1,
+        CounterInstruction::Reset => counter_account.counter = 0,
+        CounterInstruction::Update(args) => counter_account.counter = args.value,
+    }
+
+    counter_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use solana_program::{clock::Epoch, pubkey::Pubkey};
+
+    #[test]
+    fn test_counter() {
+        let program_id = Pubkey::default();
+        let key = Pubkey::default();
+        let mut lamports = 0;
+        let mut data = vec![0; size_of::<u32>()];
+        let owner = Pubkey::default();
+        let account = AccountInfo::new(
+            &key,
+            false,
+            true,
+            &mut lamports,
+            &mut data,
+            &owner,
+            false,
+            Epoch::default(),
+        );
+
+        let accounts = vec![account];
+        let increment_instruction_data: Vec<u8> = vec![0];
+        let decrement_instruction_data: Vec<u8> = vec![1];
+        let mut update_instruction_data: Vec<u8> = vec![2];
+        let reset_instruction_data: Vec<u8> = vec![3];
+
+        process_instruction(&program_id, &accounts, &increment_instruction_data).unwrap();
+        assert_eq!(
+            CounterAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            1
+        );
+
+        process_instruction(&program_id, &accounts, &decrement_instruction_data).unwrap();
+        assert_eq!(
+            CounterAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            0
+        );
+
+        let update_value = 33u32;
+
+        update_instruction_data.extend_from_slice(&update_value.to_le_bytes());
+
+        process_instruction(&program_id, &accounts, &update_instruction_data).unwrap();
+
+        assert_eq!(
+            CounterAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            33
+        );
+
+        process_instruction(&program_id, &accounts, &reset_instruction_data).unwrap();
+
+        assert_eq!(
+            CounterAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            0
+        );
+    }
+}
